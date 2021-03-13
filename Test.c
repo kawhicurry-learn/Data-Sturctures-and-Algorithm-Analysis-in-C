83680f8e132297b1ae9266a454884e9001f59ff6
kawhicurry <68467656+kawhicurry@users.noreply.github.com>
2021-03-13 23:21:06 +0800

    3/13

--- Test.c
+++ Test.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main() {
-  printf("hello,world!");
-  return 0;
+int main( ) {
+    int i;
+    while (scanf("%d", &i) != EOF) {
+        if (i % 2 != 0) {
+            i = (1 + i) / 2 * i;
+        } else {
+            i = i / 2 * (1 + i);
+        }
+        printf("%d\n\n", i);
+    }
+    return 0;
 }
-//test for git
+// test for git
